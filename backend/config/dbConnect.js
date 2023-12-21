6a5db178fcf8623dbb3929fcc2858d32ef471fc2
Mikle Gromov <moygromov@yandex.ru>
2023-12-21 10:31:34 +0300

    build

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 export async function dbConnect() {
     if (mongoose.connection.readyState >= 1) {
-        return mongoose.connection.asPromise();
+        return Promise.resolve(); // Возвращаем завершенный промис, так как соединение уже установлено
     }
 
     mongoose.set("strictQuery", false);
-    console.log("db______________");
-    await mongoose
-        .connect(process.env.DB_URI)
-        .then(() => console.log("Connected!"));
+    console.log("db!!!");
+
+    // Ждем завершения подключения перед возвратом промиса
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Connected!");
 }

6c1b7e6c75657613e37445a40a46b4dd6e24f868
Mikle Gromov <moygromov@yandex.ru>
2023-12-29 23:07:05 +0300

    multer 02

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -1,11 +1,18 @@
 import multer from "multer";
 import fs from "fs";
+import path from "path";
 
-const uploadDirectory = "public/uploads";
+// Определение директории в зависимости от окружения
+const isVercelDev = process.env.VERCEL_ENV === "development";
+
+// Определение директории в зависимости от окружения
+const uploadDirectory = isVercelDev
+    ? path.join(process.cwd(), "public/images/uploads")
+    : path.join("/tmp", "uploads");
 
 // Проверка и создание директории, если её нет
 if (!fs.existsSync(uploadDirectory)) {
-    fs.mkdirSync(uploadDirectory);
+    fs.mkdirSync(uploadDirectory, { recursive: true });
 }
 
 const storage = multer.diskStorage({

f53d8f72c08e2f49f5eaf50950919e5b519f274a
azimuth-mo <azimuth-m@yandex.ru>
2024-07-31 17:31:43 +0300

    3.0.0.0 -really big bug

--- app/landings/canyon/page.jsx
+++ app/landings/canyon/page.jsx
@@ -3,7 +3,6 @@ import Header from "@/components/landings/canyon/components/Header/Header";
 import SpecialMenu from "@/components/landings/canyon/components/Menu/SpecialMenu";
 import AboutUs from "@/components/landings/canyon/components/AboutUs/AboutUs";
 import Chef from "@/components/landings/canyon/components/Chef/Chef";
-import Intro from "@/components/landings/canyon/components/Intro/Intro";
 import Gallery from "@/components/landings/canyon/components/Gallery/Gallery";
 import FindUs from "@/components/landings/canyon/components/FindUs/FindUs";
 import Footer from "@/components/landings/canyon/components/layout/Footer/Footer";
@@ -26,7 +25,6 @@ export default function Canyon() {
             <AboutUs />
             <SpecialMenu />
             <Chef />
-            {/* <Intro /> */}
             <Gallery />
             <FindUs />
             <Footer />
